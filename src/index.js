d7c3ce839629f0a2cd996633135547bc540eca02
Joel Mwaga <mwagahjoel14@gmail.com>
2025-03-18 12:18:06 +0300

    changed 1 file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const taskInput = document.getElementById('new-task-description');
+    const taskText = taskInput.value;
+
+    if (taskText) {
+      const listItem = document.createElement('li');
+      listItem.textContent = taskText;
+      taskList.appendChild(listItem);
+      taskInput.value = '';
+    }
+  });
 });
